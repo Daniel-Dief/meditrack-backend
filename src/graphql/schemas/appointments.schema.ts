8d6e7c3aa4390b6ff4dae99aa1be7c2229fda533
Daniel Santos <danieldiefsantos@gmail.com>
2025-05-20 14:52:18 -0300

    feat: Criando mutations de CRUD para a tabela "appointments"

--- src/graphql/schemas/appointments.schema.ts
+++ src/graphql/schemas/appointments.schema.ts
@@ -15,11 +15,31 @@ export const AppointmentsSchema = gql`
         Patient: Patient!
         Doctor: Doctor!
         Status: Status!
-        Consultation: Consultation!
+        Consultation: Consultation
+    }
+
+    input CreateAppointmentInput {
+        AppointmentDate: DateTime!
+        PatientId: ID!
+        DoctorId: ID!
+        StatusId: ID!
+    }
+
+    input UpdateAppointmentInput {
+        AppointmentDate: DateTime
+        PatientId: ID
+        DoctorId: ID
+        StatusId: ID
     }
 
     extend type Query {
         allAppointments: [Appointment!]!
         appointment (id: ID!): Appointment
+    }
+
+    extend type Mutation {
+        createAppointment(input: CreateAppointmentInput!): Appointment!
+        updateAppointment(id: ID!, input: UpdateAppointmentInput!): Appointment!
+        deleteAppointment(id: ID!): Boolean!
     }
 `
